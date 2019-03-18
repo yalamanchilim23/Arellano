ec4b875b721bd5dc0a9eb4eac6fe8ddcdac20d39
yalamanchilim23 <48689250+yalamanchilim23@users.noreply.github.com>
2019-03-18 13:17:15 -0500

    Add files via upload

---
+++ dist/js/main.js
@@ -0,0 +1,179 @@
+function showAnswer() {
+// Get the id of the answer col
+answer = document.getElementById('answer');
+// Change its display property to something aside from 'none'
+answer.style.display = 'block';
+
+// Repeat the process for the explanation col!
+
+
+// Log a message to the console to make sure it all works!
+console.log(`Made an answer visible for the user.`);
+
+// Could you do the same thing yourself for the hide?
+}
+
+function hideAnswer() {
+  answ = document.getElementById('answer');
+  answ.style.display = 'none';
+}
+
+function showAnswer2() {
+// Get the id of the answer col
+answer = document.getElementById('answer_2');
+// Change its display property to something aside from 'none'
+answer.style.display = 'block';
+
+// Repeat the process for the explanation col!
+
+
+// Log a message to the console to make sure it all works!
+console.log(`Made an answer visible for the user.`);
+
+// Could you do the same thing yourself for the hide?
+}
+
+function hideAnswer2() {
+  answ = document.getElementById('answer_2');
+  answ.style.display = 'none';
+}
+
+function showAnswer3() {
+// Get the id of the answer col
+answer = document.getElementById('answer_3');
+// Change its display property to something aside from 'none'
+answer.style.display = 'block';
+
+// Repeat the process for the explanation col!
+
+
+// Log a message to the console to make sure it all works!
+console.log(`Made an answer visible for the user.`);
+
+// Could you do the same thing yourself for the hide?
+}
+
+function hideAnswer3() {
+  answ = document.getElementById('answer_3');
+  answ.style.display = 'none';
+}
+
+function showAnswer4() {
+// Get the id of the answer col
+answer = document.getElementById('answer_4');
+// Change its display property to something aside from 'none'
+answer.style.display = 'block';
+
+// Repeat the process for the explanation col!
+
+
+// Log a message to the console to make sure it all works!
+console.log(`Made an answer visible for the user.`);
+
+// Could you do the same thing yourself for the hide?
+}
+
+function hideAnswer4() {
+  answ = document.getElementById('answer_4');
+  answ.style.display = 'none';
+}
+
+function showAnswer4() {
+// Get the id of the answer col
+answer = document.getElementById('answer_4');
+// Change its display property to something aside from 'none'
+answer.style.display = 'block';
+
+// Repeat the process for the explanation col!
+
+
+// Log a message to the console to make sure it all works!
+console.log(`Made an answer visible for the user.`);
+
+// Could you do the same thing yourself for the hide?
+}
+
+function hideAnswer4() {
+  answ = document.getElementById('answer_4');
+  answ.style.display = 'none';
+}
+
+function showAnswer5() {
+// Get the id of the answer col
+answer = document.getElementById('answer_5');
+// Change its display property to something aside from 'none'
+answer.style.display = 'block';
+
+// Repeat the process for the explanation col!
+
+
+// Log a message to the console to make sure it all works!
+console.log(`Made an answer visible for the user.`);
+
+// Could you do the same thing yourself for the hide?
+}
+
+function hideAnswer5() {
+  answ = document.getElementById('answer_5');
+  answ.style.display = 'none';
+}
+
+function showAnswer6() {
+// Get the id of the answer col
+answer = document.getElementById('answer_6');
+// Change its display property to something aside from 'none'
+answer.style.display = 'block';
+
+// Repeat the process for the explanation col!
+
+
+// Log a message to the console to make sure it all works!
+console.log(`Made an answer visible for the user.`);
+
+// Could you do the same thing yourself for the hide?
+}
+
+function hideAnswer6() {
+  answ = document.getElementById('answer_6');
+  answ.style.display = 'none';
+}
+
+function showAnswer6() {
+// Get the id of the answer col
+answer = document.getElementById('answer_6');
+// Change its display property to something aside from 'none'
+answer.style.display = 'block';
+
+// Repeat the process for the explanation col!
+
+
+// Log a message to the console to make sure it all works!
+console.log(`Made an answer visible for the user.`);
+
+// Could you do the same thing yourself for the hide?
+}
+
+function hideAnswer6() {
+  answ = document.getElementById('answer_6');
+  answ.style.display = 'none';
+}
+
+function showAnswer7() {
+// Get the id of the answer col
+answer = document.getElementById('answer_7');
+// Change its display property to something aside from 'none'
+answer.style.display = 'block';
+
+// Repeat the process for the explanation col!
+
+
+// Log a message to the console to make sure it all works!
+console.log(`Made an answer visible for the user.`);
+
+// Could you do the same thing yourself for the hide?
+}
+
+function hideAnswer7() {
+  answ = document.getElementById('answer_7');
+  answ.style.display = 'none';
+}
